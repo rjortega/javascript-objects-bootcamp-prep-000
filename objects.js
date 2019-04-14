5ef63a36be962447942f177567d7e563fdd3b521
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-14 19:21:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist : "song"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName'] = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
 
